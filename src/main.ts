e00fded94c00e9d38d1179fd51063fd9b7c302a2
iyansr <ikhyan7@gmail.com>
2023-11-04 18:44:13 +0800

    ada

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,9 @@ app.get("/aaa", (_req, res) => {
 });
 
 if (import.meta.env.PROD) {
-  app.listen(3000);
-  console.log(`listening on http://localhost:${3000}/`);
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT);
+  console.log(`listening on http://localhost:${PORT}/`);
 }
 
 export const viteNode = app;
